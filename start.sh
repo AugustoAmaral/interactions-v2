1c41d33e64f3bce5cfa0e8436eeb9b72216b21b1
InteractionsServer <guto_wow@hotmail.com>
2024-02-01 17:27:41 +0000

    Initial Steup

--- start.sh
+++ start.sh
@@ -7,4 +7,6 @@ if ! grep -q "eula=true" eula.txt; then
         echo
     fi
 fi
-"jre/jdk8u302-b08-jre/bin/java" -javaagent:log4jfix/Log4jPatcher-1.0.0.jar -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -Xmx6144M -Xms4096M -jar forge-1.12.2-14.23.5.2847-universal.jar nogui
+# java -Xmx12288M -Xms4250M -jar forge-1.12.2-14.23.5.2847-universal.jar nogui
+
+java -javaagent:log4jfix/Log4jPatcher-1.0.0.jar -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -Xmx12288M -Xms4250M -jar forge-1.12.2-14.23.5.2847-universal.jar nogui
